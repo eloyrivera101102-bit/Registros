97bfa3c67b3e2c286c0c0b88660c1f7ccb380ef4
eloyrivera101102-bit <eloyrivera101102@gmail.com>
2026-08-12 20:55:29 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "libreta-v2";
+const CACHE = "libreta-v3";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
